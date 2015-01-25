4494fc629c37f52a38f4465400ab0a00ce539ddf
Muaz Khan <muazkh@gmail.com>
2015-01-25 10:41:16 +0500

    desktopCapture-p2p extension is updated.
    https://chrome.google.com/webstore/detail/webrtc-desktop-sharing/nkemblooioekjnpfekmjhpgkackcajhg

--- desktopCapture-p2p/desktop-capturing.js
+++ desktopCapture-p2p/desktop-capturing.js
@@ -18,14 +18,14 @@ function contextMenuSuccessCallback() {
 
     function captureDesktop() {
         if (connection && connection.attachStreams[0]) {
-            connection.attachStreams[0].onended = function() {};
+            //connection.attachStreams[0].onended = function() {};
             connection.attachStreams[0].stop();
-            setDefaults();
-            return;
+            // setDefaults();
         }
 
         var desktop_id = chrome.desktopCapture.chooseDesktopMedia(
-            ['screen', 'window'], function(chromeMediaSourceId) {
+            ['screen', 'window'],
+            function(chromeMediaSourceId) {
                 onAccessApproved(chromeMediaSourceId);
 
                 chrome.contextMenus.update(contextMenuID, {
@@ -49,8 +49,14 @@ function contextMenuSuccessCallback() {
                     chromeMediaSource: 'desktop',
                     chromeMediaSourceId: chromeMediaSourceId,
                     maxWidth: screen.width > 1920 ? screen.width : 1920,
-                    maxHeight: screen.height > 1080 ? screen.height : 1080
-                }
+                    maxHeight: screen.height > 1080 ? screen.height : 1080,
+                    minFrameRate: 30,
+                    maxFrameRate: 64,
+                    minAspectRatio: 1.77,
+                    googLeakyBucket: true,
+                    googTemporalLayeredScreencast: true
+                },
+                optional: []
             }
         }, gotStream, getUserMediaError);
 
@@ -103,15 +109,35 @@ function contextMenuSuccessCallback() {
         // www.RTCMultiConnection.org/docs/
         connection = new RTCMultiConnection();
 
+        connection.optionalArgument = {
+            optional: [{
+                DtlsSrtpKeyAgreement: true
+            }, {
+                googImprovedWifiBwe: true
+            }, {
+                googScreencastMinBitrate: 300
+            }, {
+                googIPv6: true
+            }, {
+                googDscp: true
+            }, {
+                googCpuUnderuseThreshold: 55
+            }, {
+                googCpuOveruseThreshold: 85
+            }, {
+                googSuspendBelowMinBitrate: true
+            }, {
+                googCpuOveruseDetection: true
+            }],
+            mandatory: {}
+        };
+
         connection.channel = connection.sessionid = connection.userid;
 
         connection.autoReDialOnFailure = true;
         connection.getExternalIceServers = false;
 
-        // www.RTCMultiConnection.org/docs/bandwidth/
-        connection.bandwidth = {
-            screen: 300 // 300kbps
-        };
+        setBandwidth(connection);
 
         // www.RTCMultiConnection.org/docs/session/
         connection.session = {
@@ -142,6 +168,8 @@ function contextMenuSuccessCallback() {
         var domain = 'https://www.webrtc-experiment.com';
         var resultingURL = domain + '/view/?sessionid=' + connection.sessionid;
 
+        // var resultingURL = 'http://localhost:1111/Chrome-Extensions/desktopCapture-p2p/viewer.html?sessionid=' + connection.sessionid;
+
         chrome.windows.create({
             url: "data:text/html,<h1>Copy Following Private URL:</h1><input type='text' value='" + resultingURL + "' style='width:100%;font-size:1.2em;'><br>You can share this private-session URI with fellows using email or social networks.",
             type: 'popup',
@@ -155,6 +183,7 @@ function contextMenuSuccessCallback() {
     // using websockets for signaling
 
     var webSocketURI = 'wss://wsnodejs.nodejitsu.com:443';
+    // var webSocketURI = 'ws://localhost:8888';
 
     function openSignalingChannel(config) {
         config.channel = config.channel || this.channel;
@@ -186,13 +215,13 @@ function contextMenuSuccessCallback() {
             }));
         };
     }
-    
+
     function setDefaults() {
         if (connection) {
             connection.close();
             connection.attachStreams = [];
         }
-        
+
         chrome.browserAction.setIcon({
             path: 'images/desktopCapture22.png'
         });
@@ -215,5 +244,66 @@ function contextMenuSuccessCallback() {
 
             popup_id = null;
         }
+    }
+}
+
+function setBandwidth(connection) {
+    // www.RTCMultiConnection.org/docs/bandwidth/
+    connection.bandwidth = {
+        screen: 300 // 300kbps
+    };
+
+    connection.processSdp = function(sdp) {
+        sdp = setSendBandwidth(sdp);
+        return sdp;
+    };
+
+    function setSendBandwidth(sdp) {
+        var sdpLines = sdp.split('\r\n');
+
+        // VP8
+        var vp8Index = findLine(sdpLines, 'a=rtpmap', 'VP8/90000');
+        var vp8Payload;
+        if (vp8Index) {
+            vp8Payload = getCodecPayloadType(sdpLines[vp8Index]);
+        }
+
+        var rtxIndex = findLine(sdpLines, 'a=rtpmap', 'rtx/90000');
+        var rtxPayload;
+        if (rtxIndex) {
+            rtxPayload = getCodecPayloadType(sdpLines[rtxIndex]);
+        }
+
+        var rtxFmtpLineIndex = findLine(sdpLines, 'a=fmtp:' + rtxPayload.toString());
+        if (rtxFmtpLineIndex !== null) {
+            var appendrtxNext = '\r\n';
+            appendrtxNext += 'a=fmtp:' + vp8Payload + ' x-google-min-bitrate=300; x-google-max-bitrate=300';
+            sdpLines[rtxFmtpLineIndex] = sdpLines[rtxFmtpLineIndex].concat(appendrtxNext);
+            sdp = sdpLines.join('\r\n');
+        }
+        return sdp;
+    }
+
+    function findLine(sdpLines, prefix, substr) {
+        return findLineInRange(sdpLines, 0, -1, prefix, substr);
+    }
+
+    function findLineInRange(sdpLines, startLine, endLine, prefix, substr) {
+        var realEndLine = endLine !== -1 ? endLine : sdpLines.length;
+        for (var i = startLine; i < realEndLine; ++i) {
+            if (sdpLines[i].indexOf(prefix) === 0) {
+                if (!substr ||
+                    sdpLines[i].toLowerCase().indexOf(substr.toLowerCase()) !== -1) {
+                    return i;
+                }
+            }
+        }
+        return null;
+    }
+
+    function getCodecPayloadType(sdpLine) {
+        var pattern = new RegExp('a=rtpmap:(\\d+) \\w+\\/\\d+');
+        var result = sdpLine.match(pattern);
+        return (result && result.length === 2) ? result[1] : null;
     }
 }
