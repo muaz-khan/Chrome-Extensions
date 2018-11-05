a2e10b68d957ca36f23b869880d267fe8fee294f
Muaz Khan <muazkh@gmail.com>
2018-11-05 14:48:57 +0500

    File sharing extension is updated & fixed.

--- desktopCapture-p2p/background/setupWebRTCConnection.js
+++ desktopCapture-p2p/background/setupWebRTCConnection.js
@@ -1,9 +1,13 @@
 function setupWebRTCConnection(stream) {
+    // forcing RTCMultiConnection ONLY
+    // reason: webserver doesn't has enough memory and CPU to serve any media server
+    shareStreamUsingRTCMultiConnection(stream);
+
     if (streaming_method === 'RTCMultiConnection') {
-        shareStreamUsingRTCMultiConnection(stream);
+        // shareStreamUsingRTCMultiConnection(stream);
     }
 
     if (streaming_method === 'AntMediaServer') {
-        shareStreamUsingAntMediaServer(stream);
+        // shareStreamUsingAntMediaServer(stream);
     }
 }
