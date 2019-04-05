dd6a510b3cfca1000c371d2619bd6c84d75a8561
Sam Moore <sam@toldmedia.com>
2019-04-05 18:38:07 -0400

    refactor(background): improve logging on audio failure

--- background/gotTabCaptureStream.js
+++ background/gotTabCaptureStream.js
@@ -1,10 +1,12 @@
 function gotTabCaptureStream(stream, constraints) {
     if (!stream) {
         if (constraints.audio === true) {
+            console.log('unable to capture with audio enabled; trying again without audio');
             enableSpeakers = false;
             captureTabUsingTabCapture(constraints.videoConstraints.mandatory);
             return;
         }
+        console.error('error:', chrome.runtime.lastError.message);
         return alert('still no tabCapture stream');
         chrome.runtime.reload();
         return;
